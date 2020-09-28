1395ea844d3aeb7829257a6ee04c882bc075d704
46and2 <46and2@gmx.de>
2020-09-28 16:12:37 +0200

    refactoring ft; new method 'assert_row_in_table'

--- functional_tests.py
+++ functional_tests.py
@@ -9,59 +9,59 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver')
 
-        # user visits webpage
+        ## user visits webpage
         self.browser.get('http://localhost:8000/')
 
     def tearDown(self):
         self.browser.quit()
 
+    def assert_row_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_start_and_retrive_list(self):
-        # user notices webpage title and header mention "to-do" lists
+        ## user notices webpage title and header mention "to-do" lists
         self.assertIn('To-Do', self.browser.title)
 
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # user is invited to enter a to-item straight away
+        ## user is invited to enter a to-item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
 
         self.assertEqual(inputbox.get_attribute('placeholder'), 'enter a to-do item')
 
-        # user typer "buy new shoes" into a text box
-        inputbox.send_keys('buy new shoes')
+        ## user typer "buy new shoes" into a text box
+        todo_1 = 'buy new shoes'
+        inputbox.send_keys(todo_1)
 
-        # when user hits enter, the webpage updates and now the webpages lists:
-        # "1: buy new shoes" as an item in a to-do list
+        ## when user hits enter, the webpage updates and now the webpages lists:
+        ## "1: buy new shoes" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
+        self.assert_row_in_table(f'1: {todo_1}')
 
-        rows = table.find_elements_by_tag_name('tr')
-
-        # self.assertTrue(any(row.text == '1: buy new shoes' for row in rows),
-        #                 f'new item did not appear in table; contents were:\n{table.text}')
-        self.assertIn('1: buy new shoes', [row.text for row in rows])
-
-        # there is still a text box inviting the user to add another item
-        # user enters "check shoes"
+        ## there is still a text box inviting the user to add another item
+        ## user enters "check shoes"
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('check shoes')
+        todo_2 = 'check_shoes'
+        inputbox.send_keys(todo_2)
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
 
-        self.assertIn('1: buy new shoes', [row.text for row in rows])
-        self.assertIn('2: check shoes', [row.text for row in rows])
+        self.assert_row_in_table(f'1: {todo_1}')
+        self.assert_row_in_table(f'2: {todo_2}')
 
+        ## the webpage updates again and now shows both items on her list
 
-        # the webpage updates again and now shows both items on her list
+        ## user wonders if the webpage will remember his/hers list
+        ## then user sees that the webpage ahs generated a uqique URL for him/her
 
-        # user wonders if the webpage will remember his/hers list
-        # then user sees that the webpage ahs generated a uqique URL for him/her
-
-        # user visits that URL; user's to-do list is still there
+        ## user visits that URL; user's to-do list is still there
 
 
         self.fail('finish the test!')
