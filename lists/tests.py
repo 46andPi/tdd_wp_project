5acfbbc1c2a647afa8a2dba37822d288bb0055f6
46and2 <46and2@gmx.de>
2020-10-03 22:13:13 +0200

    removed outdated unittest

--- lists/tests.py
+++ lists/tests.py
@@ -1,16 +1,9 @@
 from django.test import TestCase
-from django.urls import resolve
 
-from lists.views import home_page
 from lists.models import Item
 
 
 class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-
-        self.assertEqual(found.func, home_page)
 
     def test_uses_home_template(self):
         response = self.client.get('/')
