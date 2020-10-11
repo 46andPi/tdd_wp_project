d2a0ca4392ddf13a3cb0e7ceadab21aaee501964
46and2 <46and2@gmx.de>
2020-10-11 15:50:45 +0200

    removed DRIVER_PATH for Chrome

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,7 +7,7 @@ import os
 
 
 MAX_WAIT = 10
-DRIVER_PATH = '/usr/lib/chromium-browser/chromedriver'
+# DRIVER_PATH = '/usr/lib/chromium-browser/chromedriver'
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
@@ -24,7 +24,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         if staging_server:
             self.live_server_url = f'http://{staging_server}'
 
-        self.browser = webdriver.Chrome(executable_path=DRIVER_PATH)
+        # self.browser = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.browser = webdriver.Chrome()
         self.browser.get(self.live_server_url)
 
     def assert_row_in_table(self, row_text):
